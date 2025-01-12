405a060ab6040a9b59555e5e0295818f40f8f300
davwright3 <dwright3@ualr.edu>
2025-01-12 13:32:11 -0600

    Jan 12 Update

--- OOTP Unity Tournament Project/Assets/Scripts/JsonReader.cs
+++ OOTP Unity Tournament Project/Assets/Scripts/JsonReader.cs
@@ -8,7 +8,10 @@ using UnityEngine;
 
 public class JsonReader : MonoBehaviour
 {
+    public static JsonReader Instance {get; private set;}
+    
     public TextAsset textJSON;
+    public TextAsset ratingsTextJSON;
 
     [SerializeField] TextMeshProUGUI playerNameText;
     [SerializeField] private int playerListSize;
@@ -51,19 +54,86 @@ public class JsonReader : MonoBehaviour
     }
 
     [System.Serializable]
+    public class PositionRatings
+    {
+        public int contact_max;
+        public int contact_min;
+        public int gap_max;
+        public int gap_min;
+        public int power_max;
+        public int power_min;
+        public int eye_max;
+        public int eye_min;
+        public int avk_max;
+        public int avk_min;
+        public int babip_max;
+        public int babip_min;
+        public int l_contact_max;
+        public int l_contact_min;
+        public int l_gap_max;
+        public int l_gap_min;
+        public int l_power_max;
+        public int l_power_min;
+        public int l_eye_max;
+        public int l_eye_min;
+        public int l_avk_max;
+        public int l_avk_min;
+        public int l_babip_max;
+        public int l_babip_min;
+        public int r_contact_max;
+        public int r_contact_min;
+        public int r_gap_max;
+        public int r_gap_min;
+        public int r_power_max;
+        public int r_power_min;
+        public int r_eye_max;
+        public int r_eye_min;
+        public int r_avk_max;
+        public int r_avk_min;
+        public int r_babip_max;
+        public int r_babip_min;
+        public int speed_max;
+        public int speed_min;
+        public int steal_rate_max;
+        public int steal_rate_min;
+        public int steal_max;
+        public int steal_min;
+        public int sac_bunt_max;
+        public int sac_bunt_min;
+        public int bunt_for_hit_max;
+        public int bunt_for_hit_min;
+
+    }
+
+    [System.Serializable]
     public class PlayerList
     {
         public Player[] players;
     }
 
+    [System.Serializable]
+    public class PositionRatingsList
+    {
+        public PositionRatings[] ratings;
+    }
+
     public PlayerList myPlayerList = new PlayerList();
+    public PositionRatingsList myPositionRatingsList = new PositionRatingsList();
 
     void Awake()
     {
+        if(Instance != null)
+        {
+            Debug.LogError("More than one JSON Reader exits!" + Instance);
+            Destroy(gameObject);
+        }
+        Instance = this;
+        
         myPlayerList = JsonUtility.FromJson<PlayerList>(textJSON.text);  
         playerListSize = myPlayerList.players.Length;
-          
 
+        myPositionRatingsList = JsonUtility.FromJson<PositionRatingsList>(ratingsTextJSON.text);
+     
     }
 
     private void UpdatePlayerList()
--- OOTP Unity Tournament Project/Assets/Scripts/UI/DisplayController.cs
+++ OOTP Unity Tournament Project/Assets/Scripts/UI/DisplayController.cs
@@ -1,20 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class DisplayController : MonoBehaviour
 {
     [SerializeField] private JsonReader jsonReader;
     
     [SerializeField] private TextMeshProUGUI playerNameText;
-    [SerializeField] private TextMeshProUGUI playerAverageText;
-
-    [SerializeField] float playerAverage;
-
-    [SerializeField] string playerAverageString; 
+    
+    [SerializeField] private GameObject playeAverageBlock;
+    [SerializeField] private GameObject playerObpBlock;
+    [SerializeField] private GameObject playerSlgBlock;
+    [SerializeField] private GameObject playerOpsBlock;
+    [SerializeField] private GameObject playerWobaBlock;
+    [SerializeField] private GameObject playerKBlock;
+    [SerializeField] private GameObject playerBbBlock;
+  
+    private string playerAverageString; 
+    private int playerAverageRank;
+    private string playerOnBasePctString;
+    private int playerOnBasePctRank;
+    private string playerSlgString;
+    private int playerSlgRank;
+    private string playerOpsString;
+    private int playerOpsRank;
+    private string playerWobaString;
+    private int playerWobaRank;
+    private string playerKpctString;
+    private int playerKpctRank;
+    private string playerBbpctString;
+    private int playerBpctRank;
+    
+    
 
     [SerializeField] int currentPlayer = 4;
 
@@ -34,15 +57,80 @@ public class DisplayController : MonoBehaviour
     void ReadStats()
     {
         playerAverageString = FormatRateStat(jsonReader.myPlayerList.players[currentPlayer].avg, 4);
+        playerAverageRank = (int)jsonReader.myPlayerList.players[currentPlayer].avgRank;
+        
+        playerOnBasePctString = FormatRateStat(jsonReader.myPlayerList.players[currentPlayer].obp, 4);
+        playerOnBasePctRank = (int)jsonReader.myPlayerList.players[currentPlayer].obpRank;
+
+        playerSlgString = FormatRateStat(jsonReader.myPlayerList.players[currentPlayer].slg, 4);
+        playerSlgRank = (int)jsonReader.myPlayerList.players[currentPlayer].slgRank;
+
+        playerOpsString = FormatRateStat(jsonReader.myPlayerList.players[currentPlayer].ops, 4);
+        playerOpsRank = (int)jsonReader.myPlayerList.players[currentPlayer].opsRank;
+
+        playerWobaString = FormatRateStat(jsonReader.myPlayerList.players[currentPlayer].woba, 4);
+        playerWobaRank = (int)jsonReader.myPlayerList.players[currentPlayer].wobaRank;
+
+        playerKpctString = FormatPctStat(jsonReader.myPlayerList.players[currentPlayer].kp, 2);
+        playerKpctRank = (int)jsonReader.myPlayerList.players[currentPlayer].kRank;
+
+        playerBbpctString = FormatPctStat(jsonReader.myPlayerList.players[currentPlayer].bbp, 2);
+        playerBpctRank = (int)jsonReader.myPlayerList.players[currentPlayer].bbpkRank;
 
     }
     
     void DisplayStats()
     {
         playerNameText.text = jsonReader.myPlayerList.players[currentPlayer].title;
-        playerAverageText.text = String.Format("AVG: {0:F3}" ,playerAverageString);
+        FillStatBlock(playerAverageString, playerAverageRank, playeAverageBlock);
+        FillStatBlock(playerOnBasePctString, playerOnBasePctRank, playerObpBlock);
+        FillStatBlock(playerSlgString, playerSlgRank, playerSlgBlock);
+        FillStatBlock(playerOpsString, playerOpsRank, playerOpsBlock);
+        FillStatBlock(playerWobaString, playerWobaRank, playerWobaBlock);
+        FillPctStatBlock(playerKpctString, playerKpctRank, playerKBlock);
+        FillPctStatBlock(playerBbpctString, playerBpctRank, playerBbBlock);
+        
     }
 
+    private void FillStatBlock(string statToDisplay, int statRank, GameObject statBlock)
+    {
+        TextMeshProUGUI textBox = statBlock.GetComponentInChildren<TextMeshProUGUI>();
+        
+        textBox.text = String.Format("{0}  ({1})", statToDisplay, statRank);
+
+        Image image = statBlock.GetComponentInChildren<Image>();
+        if(statRank <=3)
+        {
+            image.color = Color.green;
+        }
+        else
+        {
+            image.GetComponent<Image>().color = Color.white;
+        }
+        
+        
+    }
+
+    private void FillPctStatBlock(string statToDisplay, int statRank, GameObject statBlock)
+    {
+        TextMeshProUGUI textBox = statBlock.GetComponentInChildren<TextMeshProUGUI>();
+        
+        textBox.text = String.Format("{0}%  ({1})", statToDisplay, statRank);
+
+        Image image = statBlock.GetComponentInChildren<Image>();
+        if(statRank <=3)
+        {
+            image.color = Color.green;
+        }
+        else
+        {
+            image.GetComponent<Image>().color = Color.white;
+        }
+        
+        
+    }
+    
+    
     private string FormatRateStat(float statToFormat, int decimalPlaces)
     {
         //local variable to manipulate
@@ -68,6 +156,23 @@ public class DisplayController : MonoBehaviour
         {
             statToFormatString = statToFormatString + "0";
         }
+
+
+        return statToFormatString;
+    }
+
+    private string FormatPctStat(float statToFormat, int decimalPlaces)
+    {
+        //local variable to manipulate
+        string statToFormatString;
+
+        //read in the stat to the method and round it to 4, since rounding to 3 will end up with too few digits for the rate stat string
+        statToFormat = (float)System.Math.Round(statToFormat * 100, decimalPlaces);
+
+        statToFormatString = statToFormat.ToString();
+
+        //cast the stat to a string for display
+        
 
 
         return statToFormatString;
